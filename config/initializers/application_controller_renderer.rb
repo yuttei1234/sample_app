307ca2c8852d832f8088fe121be5a0716ebd39ca
EC2 Default User <ec2-user@ip-172-31-14-28.ec2.internal>
2021-08-01 07:24:30 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '559a33fd2f58e246656cb4ba4327908bf4f187304bd2e72e4afef6ce05fdb0c5ccd1773eda14e3c7b28199a391fae74c5f1ae0c0b6fdd1d4bda86512a7662261'
---
+++ db/migrate/20210712232118_remove_name_from_list.rb
@@ -0,0 +1,5 @@
+class RemoveNameFromList < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :lists, :name, :string
+  end
+end
